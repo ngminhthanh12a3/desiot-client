ce05cecc24ed1942678b486847d798172782d625
ngminhthanh12a3 <ngminhthanh12a3@gmail.com>
2023-07-13 07:37:32 +0000

    1.16.0@1.16.0-server

--- src/pages/configuration/profile/index.tsx
+++ src/pages/configuration/profile/index.tsx
@@ -1,6 +1,12 @@
+import { RouteContext } from '@ant-design/pro-components';
 import { PageContainer } from '@ant-design/pro-layout';
-import { FC } from 'react';
+import { Descriptions, Typography } from 'antd';
+import { FC, useEffect } from 'react';
 import { history, useModel, useRequest } from 'umi';
+import styles from './style.less';
+
+const { Text } = Typography;
+
 const tabList = [
   {
     key: 'device',
@@ -22,7 +28,12 @@ type ProfileParamsType = {
 const Profile: FC<API.DESIoTPropsType<ProfileParamsType>> = (props) => {
   const { config_id } = props.match.params;
   const { setConfig_id } = useModel('UI');
-  setConfig_id(config_id);
+  useEffect(() => {
+    setConfig_id(config_id);
+
+    return () => {};
+  }, []);
+
   const { data = { name: '' }, loading } = useRequest('/api/configs/' + config_id);
 
   const getTabKey = () => {
@@ -40,6 +51,19 @@ const Profile: FC<API.DESIoTPropsType<ProfileParamsType>> = (props) => {
     const url = match.url === '/' ? '' : match.url;
     history.push(`${url}/${key}`, { config_id });
   };
+
+  const description = (
+    <RouteContext.Consumer>
+      {({ isMobile }) => (
+        <Descriptions className={styles.headerList} size="small" column={isMobile ? 1 : 2}>
+          <Descriptions.Item label="ID">
+            <Text copyable>{config_id}</Text>
+          </Descriptions.Item>
+        </Descriptions>
+      )}
+    </RouteContext.Consumer>
+  );
+
   return (
     <PageContainer
       title={data.name}
@@ -47,6 +71,7 @@ const Profile: FC<API.DESIoTPropsType<ProfileParamsType>> = (props) => {
       tabList={tabList}
       tabActiveKey={getTabKey()}
       onTabChange={handleTabChange}
+      content={description}
       fixedHeader
     >
       {props.children}
